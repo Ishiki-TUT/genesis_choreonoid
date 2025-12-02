36dbd78a2241d7b5b55bb42b19274cb9d118a43b
Ishiki-TUT <ishiki.shinnosuke.ur@tut.jp>
2025-12-02 20:49:06 +0900

    friction-walking-terrain2-kp2000kd50-kpkdrand-13

--- samples/bp000_train.py
+++ samples/bp000_train.py
@@ -157,32 +157,32 @@ def get_cfgs():
             "dof_vel": 0.05,
         },
     }
-    # reward_cfg = {
-    #     "tracking_sigma": 0.25,
-    #     "base_height_target": 0.64,
-    #     "feet_height_target": 0.075,
-    #     "reward_scales": {
-    #         "tracking_lin_vel": 8.0, # 2.0
-    #         "tracking_ang_vel": 0.8, # 0.4
-    #         "lin_vel_z": -2.0,
-    #         "base_height": -5.0, # -50.0
-    #         "action_rate": -0.01,  # -0.005
-    #         "similar_to_default": -0.1,
-    #         # "effort": 0.01,
-    #         # "tracking_error": -0.1,
-    #         "episode_len": 0.1, # 0.01
-    #         "correct_action": 0.01, # 0.01
-    #         "min_ankle_height": 0.0, #10.0
-    #         "plus_watt": 0.00, # -0.01
-    #         "ankle_regularization": -0.5,
-    #         "feet_stride": 0.0, # 1.0
-    #         "hip_pitch_motion": 0.0, # 0.1
-    #         "feet_alternating_pos": 2.0, # 5.0
-    #         "dof_vel": 0.000, # -0.0005
-    #         "feet_pos_symmetry": -1.0,
-    #     },
-    # }
-    
+    reward_cfg = {
+        "tracking_sigma": 0.25,
+        "base_height_target": 0.64,
+        "feet_height_target": 0.075,
+        "reward_scales": {
+            "tracking_lin_vel": 8.0, # 2.0
+            "tracking_ang_vel": 0.8, # 0.4
+            "lin_vel_z": -2.0,
+            "base_height": -5.0, # -50.0
+            "action_rate": -0.1,  # -0.005
+            "similar_to_default": -0.1,
+            # "effort": 0.01,
+            # "tracking_error": -0.1,
+            "episode_len": 0.1, # 0.01
+            "correct_action": 0.01, # 0.01
+            "min_ankle_height": 0.0, #10.0
+            "plus_watt": 0.00, # -0.01
+            "ankle_regularization": -0.5,
+            "feet_stride": 0.0, # 1.0
+            "hip_pitch_motion": 0.0, # 0.1
+            "feet_alternating_pos": 2.0, # 5.0
+            "dof_vel": 0.000, # -0.0005
+            "feet_pos_symmetry": -1.0,
+        },
+    }
+
     # reward_cfg = {
     #     "tracking_sigma": 0.25,
     #     "base_height_target": 0.64,
@@ -204,25 +204,25 @@ def get_cfgs():
     # }
 
     # リドさんリワード設定
-    reward_cfg = {
-        "tracking_sigma": 0.25,
-        # "base_height_target": 0.64,
-        "base_height_target": 0.70,
-        "feet_height_target": 0.075,
-        "reward_scales": {
-            "tracking_lin_vel": 1.0,
-            "tracking_ang_vel": 0.2,
-            # "tracking_lin_vel": 2.0,
-            # "tracking_ang_vel": 1.6,
-            "lin_vel_z": -1.0,
-            "base_height": -50.0,
-            "action_rate": -0.005,
-            # "action_rate": -0.05,
-            "similar_to_default": -0.1,
-            # "effort": 0.,
-            # "tracking_error": -0.1,
-        },
-    }
+    # reward_cfg = {
+    #     "tracking_sigma": 0.25,
+    #     # "base_height_target": 0.64,
+    #     "base_height_target": 0.70,
+    #     "feet_height_target": 0.075,
+    #     "reward_scales": {
+    #         "tracking_lin_vel": 1.0,
+    #         "tracking_ang_vel": 0.2,
+    #         # "tracking_lin_vel": 2.0,
+    #         # "tracking_ang_vel": 1.6,
+    #         "lin_vel_z": -1.0,
+    #         "base_height": -50.0,
+    #         "action_rate": -0.005,
+    #         # "action_rate": -0.05,
+    #         "similar_to_default": -0.1,
+    #         # "effort": 0.,
+    #         # "tracking_error": -0.1,
+    #     },
+    # }
     command_cfg = {
         "num_commands": 3,
         "lin_vel_x_range": [0.5, 0.5], # [0.5, 0.5]
